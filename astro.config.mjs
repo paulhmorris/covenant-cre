77e503eb60fcb98a8a65ff25fccbac43b0bd2a49
Paul Morris <paulh.morris@gmail.com>
2025-12-10 20:59:05 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const iconsPath = "node_modules/@shoelace-style/shoelace/dist/assets/icons";
+
+export default defineConfig({
+  prefetch: { prefetchAll: true },
+  site: "https://covenantcre.com",
+  trailingSlash: "never",
+  vite: {
+    resolve: { alias: [{ find: /\/assets\/icons\/(.+)/, replacement: `${iconsPath}/$1` }] },
+    plugins: [
+      tailwindcss({ optimize: { minify: true } }),
+      viteStaticCopy({ targets: [{ src: iconsPath, dest: "assets" }] }),
+    ],
+  },
+  integrations: [sitemap(), icon()],
+});
