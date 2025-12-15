fad9c1d1e261f782f205a890906cc0842f9e82ef
Paul Morris <paulh.morris@gmail.com>
2025-12-15 12:40:57 -0600

    Finishing touches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
@@ -10,9 +11,13 @@ const iconsPath = "node_modules/@shoelace-style/shoelace/dist/assets/icons";
 export default defineConfig({
   site: "https://covenantcre.com",
   trailingSlash: "never",
+  devToolbar: { enabled: false },
+
   vite: {
     resolve: { alias: [{ find: /\/assets\/icons\/(.+)/, replacement: `${iconsPath}/$1` }] },
     plugins: [tailwindcss(), viteStaticCopy({ targets: [{ src: iconsPath, dest: "assets" }] })],
   },
+
   integrations: [sitemap(), icon()],
+  adapter: cloudflare(),
 });
